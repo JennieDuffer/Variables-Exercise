ba4f3e1c4e3aaed564f5b259fcb1259780b44444
Jennie Duffer <jenniej297@gmail.com>
2022-08-04 16:15:10 -0500

    initial commit

---
+++ Variables Exercise/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+string dogName = "Chance";
+int dogAge = 10;
+char nickName = 'C';
+bool lovesSleeping = true;
+double tennisBalls = 32;
+decimal weight = 30.4m;
+Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old");
